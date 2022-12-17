2351dfa5b7a5eb9b427f623041c5823d6778c9df
Ahmed2120 <ahmed202ashraf202@gmail.com>
2022-12-17 12:41:26 +0200

    create favorite functionality

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,6 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
         providers: [
           ChangeNotifierProvider(
-            create: (context) => FavoriteProvider(),
-          ),
-          ChangeNotifierProvider(
             create: (context) => ApiService(),
           ),
         ],
